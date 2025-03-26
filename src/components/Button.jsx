a0fa9377bc057ccbae21849c36ac701e93c5fa9c
Arungiri2021 <arun@bunity.com>
2025-03-27 00:16:24 +0530

    button components

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,28 @@
-export default function Button() {
+export default function Button(
+    {
+        classes,
+        endIcon,
+        id,
+        onClick,
+        startIcon,
+        text,
+        type,
+        isDisabled = false
+    }
+) {
+    const alertMissingHandler = () => window.alert("Attach an event handler to the Button!");
+
     return (
-        <div>
-            hee hha
-        </div>
+        <button
+            type={type ? type : "button"}
+            className={`btn ${classes ? classes : "btn-primary"}`}
+            id={id}
+            disabled={isDisabled}
+            onClick={onClick ? onClick : alertMissingHandler}
+        >
+            {startIcon}
+            {text ? text : "Click Me"}
+            {endIcon}
+        </button>
     );
 }
